a2752b54bf7564fab712833ed07a9a85d40ce7ba
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-07-24 10:40:24 +0300

    .

--- BOAPlugins/FormApplicationGenerator/Extensions.cs
+++ BOAPlugins/FormApplicationGenerator/Extensions.cs
@@ -1,15 +1,65 @@
 ﻿using System;
+using System.Windows.Controls;
+using System.Windows.Documents;
 using BOA.CodeGeneration.Generators;
 using BOA.Common.Helpers;
 
 namespace BOAPlugins.FormApplicationGenerator
 {
-    static class Extensions
+    public static class Extensions
     {
-        #region Public Methods
-        public static string GetSnapName(this FieldInfo dataField)
+        #region Methods
+        internal static string GetSnapName(this FieldInfo dataField)
         {
             return $"{dataField.ComponentName.ToString().RemoveFromStart("B").MakeLowerCaseFirstChar()}{dataField.Name}";
+        }
+
+        internal static string GetText(this RichTextBox richTextBox)
+        {
+            return new TextRange(richTextBox.Document.ContentStart,
+                                 richTextBox.Document.ContentEnd).Text;
+        }
+
+        internal static bool HasSnapName(this FieldInfo dataField)
+        {
+            return dataField.ComponentName == ComponentName.BAccountComponent ||
+                   dataField.ComponentName == ComponentName.BParameterComponent;
+        }
+
+        internal static void SetText(this RichTextBox richTextBox, string text)
+        {
+            richTextBox.Document.Blocks.Clear();
+            richTextBox.Document.Blocks.Add(new Paragraph(new Run(text)));
+        }
+
+        internal static string ToCSharp(this DotNetTypeName name)
+        {
+            if (name == DotNetTypeName.Boolean)
+            {
+                return "bool?";
+            }
+
+            if (name == DotNetTypeName.DateTime)
+            {
+                return "DateTime?";
+            }
+
+            if (name == DotNetTypeName.Decimal)
+            {
+                return "decimal?";
+            }
+
+            if (name == DotNetTypeName.Int32)
+            {
+                return "int?";
+            }
+
+            if (name == DotNetTypeName.String)
+            {
+                return "string";
+            }
+
+            throw new ArgumentException(name.ToString());
         }
 
         static string MakeLowerCaseFirstChar(this string value)
@@ -21,41 +71,6 @@ namespace BOAPlugins.FormApplicationGenerator
 
             return ContractBodyGenerator.GetPropertyFieldName("", value);
         }
-
-        public static bool HasSnapName(this FieldInfo dataField)
-        {
-            return dataField.ComponentName == ComponentName.BAccountComponent ||
-                   dataField.ComponentName == ComponentName.BParameterComponent;
-        }
-
-        public static string ToCSharp(this DotNetTypeName name)
-        {
-            if (name == DotNetTypeName.Boolean)
-            {
-                return "bool?";
-            }
-
-            if (name == DotNetTypeName.DateTime)
-            {
-                return "DateTime?";
-            }
-            if (name == DotNetTypeName.Decimal)
-            {
-                return "decimal?";
-            }
-            if (name == DotNetTypeName.Int32)
-            {
-                return "int?";
-            }
-            if (name == DotNetTypeName.String)
-            {
-                return "string";
-            }
-
-            throw new ArgumentException(name.ToString());
-        }
-        
-
         #endregion
     }
 }

89c5f2a64307b7143d2ee14f3892e5c325b7f70d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-07-25 16:16:09 +0300

    .

--- Tests/BOAPlugins.Test/FormApplicationGenerator/SolutionFile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BOAPlugins.FormApplicationGenerator
-{
-    static class SolutionFile
-    {
-        #region Constants
-        public const string CardPaymentSystem_Clearing = @"D:\work\BOA.BusinessModules\Dev\BOA.CardPaymentSystem.Clearing\BOA.CardPaymentSystem.Clearing.sln";
-        #endregion
-    }
-}
